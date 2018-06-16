f6cea02e0807442e65c56b1a58a5b7c9afb19036
Jeroen van der Hel <jeroenvdhel@gmail.com>
2018-06-16 20:22:32 +0200

    Adding image url from database to product class and view

--- App/Models/Product.php
+++ App/Models/Product.php
@@ -19,24 +19,34 @@ class Product extends \Core\Model
 	private $description;
 	private $description_en;
 	private $salesPrice;
+	private $filename;
+	private $extension;
+	private $size;
+	private $thumb_size;
 
 	// Methods
 	public static function constructFromDatabase($id)
 	{
 		$db = static::getDB();
-
 		$stmt = $db->prepare('
-			SELECT 
-				`product`.id, 
-				`product`.name, 
-				`product`.description, 
-				`product`.description_en, 
-				`product`.salesPrice
-			FROM 
+			SELECT
+				`product`.id,
+				`product`.name,
+				`product`.description,
+				`product`.description_en,
+				`product`.salesPrice,
+				`image`.filename,
+				`image`.extension,
+				`image`.size,
+				`image`.thumb_size
+			FROM
 				`product`
-			WHERE 
-				`product`.id = :id
-			');
+			  LEFT JOIN
+				`product_image` ON `product_image`.product_id = `product`.id
+			  LEFT JOIN
+				`image` ON `image`.id = `product_image`.image_id
+			WHERE
+				`product`.id = :id');
 		$stmt->bindParam(':id', $id);
 		$stmt->execute();
 		return $stmt->fetchObject('App\Models\Product');
@@ -67,5 +77,20 @@ class Product extends \Core\Model
 		return $this->salesPrice;
 	}
 
+	public function getImgUrl()
+	{
+		$fileName = $this->filename;
+		$imgExt = $this->extension;
+		return "../img/" . $fileName . "." . $imgExt;
+	}
 
+	public function getImgSize()
+	{
+		return $this->size;
+	}
+
+	public function getThumbSize()
+	{
+		return $this->thumb_size;
+	}
 }
